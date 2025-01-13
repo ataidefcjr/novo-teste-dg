e7aa76e5efa714f016a0a52d5a005b1d0ee92e36
Lucas Xavier <lsantx@gmail.com>
2025-01-13 14:37:50 -0300

    Adiciona bandeira como entrada na calculadora_desafio2.py

--- calculadora_desafio2.py
+++ calculadora_desafio2.py
@@ -1,4 +1,4 @@
-def calculadora(consumo: list, classe: str) -> tuple:
+def calculadora(consumo: list, classe: str, bandeira: str) -> tuple:
     """
     retorna uma tupla de floats contendo economia anual, economia mensal, desconto aplicado e cobertura.
     """
@@ -20,68 +20,68 @@ def calculadora(consumo: list, classe: str) -> tuple:
 if __name__ == "__main__":
     print("Testando...")
 
-    assert calculadora([1518, 1071, 968], "Industrial") == (
+    assert calculadora([1518, 1071, 968], "Industrial", "BANDEIRA VERMELHA 2") == (
         1349.86,
         112.49,
         0.12,
         0.90,
-    )  # BANDEIRA VERMELHA 2
+    ) 
 
-    assert calculadora([1000, 1054, 1100], "Residencial") == (
+    assert calculadora([1000, 1054, 1100], "Residencial", "BANDEIRA VERMELHA 1") == (
         1725.61,
         143.8,
         0.18,
         0.90
-    )  # BANDEIRA VERMELHA 1
+    )
 
-    assert calculadora([973, 629, 726], "Comercial") == (
+    assert calculadora([973, 629, 726], "Comercial", "BANDEIRA AMARELA") == (
         1097.6,
         91.47,
         0.16,
         0.90
-    )  # BANDEIRA AMARELA
+    )
 
-    assert calculadora([15000, 14000, 16000], "Industrial") == (
+    assert calculadora([15000, 14000, 16000], "Industrial", "BANDEIRA VERMELHA 1") == (
         21656.81,
         1804.73,
         0.15,
         0.95
-    )  # BANDEIRA VERMELHA 1
+    )
 
-    assert calculadora([12000, 11000, 11400], "Residencial") == (
+    assert calculadora([12000, 11000, 11400], "Residencial", "BANDEIRA VERDE") == (
         22997.8,
         1916.48,
         0.22,
         0.95
-    )  # BANDEIRA VERDE
+    )
 
-    assert calculadora([17500, 16000, 16400], "Comercial") == (
+    assert calculadora([17500, 16000, 16400], "Comercial", "BANDEIRA AMARELA") == (
         27938.08,
         2328.17,
         0.18,
         0.95
-    )  # BANDEIRA AMARELA
+    )
 
-    assert calculadora([30000, 29000, 29500], "Industrial") == (
+    assert calculadora([30000, 29000, 29500], "Industrial", "BANDEIRA VERMELHA 1") == (
         53262.07,
         4438.51,
         0.18,
         0.99
-    )  # BANDEIRA VERMELHA 1
+    )
 
-    assert calculadora([22000, 21000, 21400], "Residencial") == (
+    assert calculadora([22000, 21000, 21400], "Residencial", "BANDEIRA AMARELA") == (
         52186.84,
         4348.9,
         0.25,
         0.99
-    )  # BANDEIRA AMARELA
+    )
 
-    assert calculadora([25500, 23000, 21400], "Comercial") == (
+    assert calculadora([25500, 23000, 21400], "Comercial", "BANDEIRA VERDE") == (
         48697.35,
         4058.11,
         0.22,
         0.99
-    )  # BANDEIRA VERDE
+    )
 
     print("Todos os testes passaram!")
     
